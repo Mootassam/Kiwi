8f944ed0c6b8d6e354089a29ee5ea83812f3243d
Mootassam <Mootassame@gmail.com>
2024-06-24 17:08:12 +0100

    add the new git

---
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -0,0 +1,29 @@
+import axios from "axios";
+import authToken from "src/modules/auth/authToken";
+
+const authAxios = axios.create({
+
+  // Local link
+  baseURL: "http://192.168.1.43:8080/api",
+
+  // louis Link
+  // baseURL: "https://www.eclicks-digital.xyz/api/", 
+
+  //kiwi LInk
+  // baseURL: "http://194.233.175.91:8080/api",
+
+  // ENd LInk
+  // baseURL: "http://172.104.141.32:8080/api",
+
+});
+
+authAxios.interceptors.request.use(async function (options) {
+  const token = authToken.get();
+  if (token) {
+    options.headers["Authorization"] = `Bearer ${token}`;
+  }
+
+  return options;
+});
+
+export default authAxios;
---
+++ graborders/src/view/pages/wallet/Wallet.tsx
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import SubHeader from "src/view/shared/Header/SubHeader";
+import yupFormSchemas from "src/modules/shared/yup/yupFormSchemas";
+import * as yup from "yup";
+import { i18n } from "../../../i18n";
+import { FormProvider, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useDispatch, useSelector } from "react-redux";
+import actions from "src/modules/auth/authActions";
+import InputFormItem from "src/shared/form/InputFormItem";
+import selector from "src/modules/auth/authSelectors";
+const schema = yup.object().shape({
+  trc20: yupFormSchemas.string(i18n("user.fields.trc20"), {
+    required: true,
+  }),
+  withdrawPassword: yupFormSchemas.string(i18n("user.fields.withdrawPassword"), {
+    required: true,
+  }),
+});
+function Wallet() {
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selector.selectCurrentUser);
+
+  const [initialValues] = useState(() => {
+    return {
+      trc20: "" || currentUser.trc20,
+      withdrawPassword: "" || currentUser.withdrawPassword,
+      walletname: "" || currentUser.walletname,
+      usernamewallet: "" || currentUser.usernamewallet,
+      balance : currentUser?.balance
+    };
+  });
+  const form = useForm({
+    resolver: yupResolver(schema),
+    mode: "onSubmit",
+    defaultValues: initialValues,
+  });
+  const onSubmit = ({ withdrawPassword, trc20, walletname, usernamewallet }) => {
+    const values = {
+      trc20: trc20,
+      walletname: walletname,
+      usernamewallet: usernamewallet,
+      balance:currentUser?.balance
+    };
+    dispatch(actions.doUpdateProfile(values));
+  };
+  return (
+    <div>
+      <SubHeader title="Wallet" path="/profile" />
+      <div className="app__wallet">
+        <div className="wallet__">
+          <h3 className="hall">Withdrawal method information</h3>
+          <FormProvider {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="wallet__form">
+                <div className="form__">
+                  <div className="form__group">
+                    <div className="label__form">
+                      <span style={{ color: "red" }}>*</span>
+                      <span style={{ fontSize: "13px" }}>Username</span>
+                    </div>
+                    <div className="input__div">
+                      <InputFormItem
+                        type="text"
+                        name="usernamewallet"
+                        placeholder={i18n("user.fields.username")}
+                        className="input__"
+                      />
+                    </div>
+                  </div>
+                  <div className="form__group">
+                    <div className="label__form">
+                      <span style={{ color: "red" }}>*</span>
+                      <span style={{ fontSize: "13px" }}>Wallet Name</span>
+                    </div>
+                    <div className="input__div">
+                      <InputFormItem
+                        type="text"
+                        name="walletname"
+                        placeholder={i18n("user.fields.walletName")}
+                        className="input__"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="form__group">
+                    <div className="label__form">
+                      <span style={{ color: "red" }}>*</span>
+                      <span style={{ fontSize: "13px" }}>USDT-TRC20</span>
+                    </div>
+                    <div className="input__div">
+                      <InputFormItem
+                        type="text"
+                        name="trc20"
+                        placeholder={i18n("user.fields.trc20")}
+                        className="input__"
+                      />
+                    </div>
+                  </div>
+                  <div className="form__group">
+                    <div className="label__form">
+                      <span style={{ color: "red" }}>*</span>
+                      <span style={{ fontSize: "13px" }}>Withdraw Password</span>
+                    </div>
+                    <div className="input__div">
+                      <InputFormItem
+                        type="text"
+                        name="withdrawPassword"
+                        placeholder={i18n("user.fields.withdrawPassword")}
+                        className="input__"
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <button
+                  className="confirm"
+                  type="submit"
+                >
+                  Submit
+                </button>
+                <span style={{ fontSize: 13 }}>
+                  <b>Note:</b> &nbsp; Please be careful when filling out this information.
+                </span>
+              </div>
+            </form>
+          </FormProvider>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Wallet;
