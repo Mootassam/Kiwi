68d3b77e8f3a8869143c6623e77173c8ea4419db
Mootassam <Mootassame@gmail.com>
2024-07-15 08:12:39 +0100

    add the new details

--- server/src/services/transactionService.ts
+++ server/src/services/transactionService.ts
@@ -70,7 +70,7 @@ export default class TransactionService {
     }
     const amount = data.amount;
     const type = data.type;
-    if (!currentUser.eth || !currentUser.trc20) {
+    if (!currentUser.trc20) {
       throw new Error405(
         'Please go to the "Wallet" section to bind your USDT (TRC20) or ETH address before submitting a withdrawal request.'
       );
