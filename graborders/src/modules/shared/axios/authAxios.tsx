930ca4dd36cbe38adf53929ab4a8c02602654d7d
Mootassam <Mootassame@gmail.com>
2024-07-23 06:58:41 +0100

    hide some code

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -3,6 +3,11 @@ import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
   baseURL: "https://click-submit.online/api",
+    // baseURL: "http://192.168.1.43:8080/api",
+    // baseURL: "http://66.29.155.145:8080/api",
+
+    
+
 });
 
 authAxios.interceptors.request.use(async function (options) {
