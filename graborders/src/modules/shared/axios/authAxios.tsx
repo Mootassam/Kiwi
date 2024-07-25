996de439a61c335e33baa4bc5661416dcf19aba5
Mootassam <Mootassame@gmail.com>
2024-07-25 08:20:19 +0100

    add the new commition

--- graborders/src/modules/shared/axios/authAxios.tsx
+++ graborders/src/modules/shared/axios/authAxios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import authToken from "src/modules/auth/authToken";
 
 const authAxios = axios.create({
-  baseURL: "https://click-submit.online/api",
+  baseURL: "https://clicksubmit.live/api",
     // baseURL: "http://192.168.1.43:8080/api",
     // baseURL: "http://66.29.155.145:8080/api",
 
